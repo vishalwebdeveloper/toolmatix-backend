32f3770b3464976c4961d5295d5e0967947e282a
Vishal Shrivastava <vishal71shrivastava@gmail.com>
2026-06-14 18:46:25 +0530

    changes in main.py

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://toolmatix.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
